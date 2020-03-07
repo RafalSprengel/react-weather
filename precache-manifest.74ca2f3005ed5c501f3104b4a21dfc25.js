af473a7c51712440350b6686827cb0740fdfc04e
Rafal66 <sprengel.rafal@gmail.com>
2020-03-07 01:00:08 +0000

    Updates

---
+++ precache-manifest.74ca2f3005ed5c501f3104b4a21dfc25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "824ace1d2dfed6bac42904dee0250c46",
+    "url": "/react-weather/index.html"
+  },
+  {
+    "revision": "0290667b6235fa84c4b5",
+    "url": "/react-weather/static/css/main.3ad3d3e2.chunk.css"
+  },
+  {
+    "revision": "ae54e08ace79aa9e3aa2",
+    "url": "/react-weather/static/js/2.dbe2e5d4.chunk.js"
+  },
+  {
+    "revision": "0290667b6235fa84c4b5",
+    "url": "/react-weather/static/js/main.0e304fcd.chunk.js"
+  },
+  {
+    "revision": "2d1d9bf4f2ce8b94fb7b",
+    "url": "/react-weather/static/js/runtime-main.a0d36e52.js"
+  }
+]);
